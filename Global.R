8dc19e6b61c5073470d14ad7a425271dd222a076
daniell2-95 <33256463+daniell2-95@users.noreply.github.com>
2019-05-08 15:37:25 -0500

    Update Global.R

--- Global.R
+++ Global.R
@@ -6,12 +6,15 @@ library(plotly)
 library(data.table)
 
 
+#GLOBAL VARIABLES
+
 trans <- fread("dh_transactions.csv", select = 
                      c("upc", "dollar_sales", "geography", "week", "coupon"), 
                    header = TRUE)
 product <- read.csv("dh_product_lookup.csv", header = TRUE)
 
-data <- sample_n(as.data.frame(merge(x=trans,y=product,by="upc")), 1000)
+#take a random subset of the data
+data <- sample_n(as.data.frame(merge(x=trans,y=product,by="upc")), 100000)
 
 geos <- unique(data$geography)
 
